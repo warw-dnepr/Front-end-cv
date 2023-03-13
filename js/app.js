98f81c964ce86e5c809f33add971d5519475a2b4
warw-dnepr <69255618+warw-dnepr@users.noreply.github.com>
2023-03-13 16:31:08 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $(function () {
+   const screenWidth = window.screen.width
+   if (screenWidth < 800) {
+      $('.block-for-img img').css(`width`, `${screenWidth}px`)
+      $('.block-for-img').css(`margin-left`, `0`).css('margin-top', '0').css('top', '0').css('left', '0')
+   }
+
    $('.diplom_1,.diplom_2').click(function () {
       $('.block-for-img,.block-for-img-child').fadeIn()
       $('.block-for-img-child').attr('src', this.id)
@@ -9,3 +15,5 @@ $(function () {
       $('.block-for-img,.block-for-img-child').fadeOut()
    })
 })
+
+
